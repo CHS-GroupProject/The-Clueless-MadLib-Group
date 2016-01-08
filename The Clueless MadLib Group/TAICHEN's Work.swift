aa7dbdf4f15ee79f489a387f94ad1ed3e83c8b8e
Coder <coder@mainlab26.local>
2016-01-08 11:07:37 -0800

    Initial

---
+++ The Clueless MadLib Group/TAICHEN's Work.swift
@@ -0,0 +1,16 @@
+//
+//  TAICHEN's Work.swift
+//  The Clueless MadLib Group
+//
+//  Created by Coder on 1/8/16.
+//  Copyright © 2016 Garrett Machen. All rights reserved.
+//
+
+import Foundation
+func input1() -> String {
+    var keyboard = NSFileHandle.fileHandleWithStandardInput()
+    var inputData = keyboard.availableData
+    var strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+    
+    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
