990b64285413652eb5546c89820e7f812957de87
Coder <coder@mainlab26.local>
2016-01-08 11:04:25 -0800

    Initial Commit

---
+++ The Clueless MadLib Group/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Clueless MadLib Group
+//
+//  Created by Coder on 1/8/16.
+//  Copyright © 2016 Garrett Machen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
